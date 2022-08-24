b82a6a4af3da80adcf3ae1e8cf3d1f683810ee16
Rick <rickatherton316@gmail.com>
2022-08-24 14:22:24 +0000

    Add Products App, Models, Fixtures & Images

---
+++ products/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=155)
+    friendly_name = models.CharField(max_length=155, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    SIZE_CHOICES = (('Yes', "Yes"), ('No', "No"))
+
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=55, unique=True)
+    name = models.CharField(max_length=155)
+    subtitle = models.CharField(max_length=205)
+    description = models.TextField()
+    has_sizes = models.CharField(max_length=3, choices=SIZE_CHOICES)
+    intensity = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.IntegerField(null=True, blank=True)
+    date_added = models.DateField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
--- tcc/settings.py
+++ tcc/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'bulma',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
