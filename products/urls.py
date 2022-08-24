63c53c0bd1c8d4474f60b569750c2c3375c30ea6
Rick <rickatherton316@gmail.com>
2022-08-24 15:26:04 +0000

    Add Products views and templates

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+"""
+Products URL Configuration
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name='products'),
+]
--- tcc/urls.py
+++ tcc/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
