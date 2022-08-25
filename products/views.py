a1234ab0ec4c0b5d99185538daa1df482ae0022e
Rick <rickatherton316@gmail.com>
2022-08-25 14:51:55 +0000

    Import Lower to aid sorting and add comments

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
@@ -12,7 +13,7 @@ def all_products(request):
     categories = None
     sort = None
     direction = None
-
+    # Handle the page request to sort products
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -21,18 +22,20 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
-                sortkey = 'category_name'            
+                sortkey = 'category__name'            
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
 
+        # Handle the page request to return a specific category
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
+        # Handle the page request to query the database
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -44,6 +47,7 @@ def all_products(request):
 
     current_sorting = f'(sort)_(direction)'
 
+    # Context to return to the page
     context = {
         'products': products,
         'search_term': query,
@@ -57,8 +61,10 @@ def all_products(request):
 def product_detail(request, product_id):
     """ A view to show a single product in detail """
 
+    # Return the requested product or 404
     product = get_object_or_404(Product, pk=product_id)
 
+    # Context to return to the page
     context = {
         'product': product,
     }
