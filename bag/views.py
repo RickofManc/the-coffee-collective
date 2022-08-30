910b0c78a99bff61da4c9e3dc7af1d15c98d2a0c
Rick <rickatherton316@gmail.com>
2022-08-30 12:21:00 +0000

    Add functionality to add Products to Bag

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,31 @@
 """
 Django imports to support Views
 """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
     """ A view to return the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add quantity and size to the bag """
+
+    quantity = int(request.POST.get('quantity'))
+    size = int(request.POST.get('size'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        bag[item_id] += size
+    else:
+        bag[item_id] = quantity
+        bag[item_id] = size
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
+
