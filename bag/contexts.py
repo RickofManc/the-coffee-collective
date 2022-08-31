6b1e80e34d53c5f67865ae1772b976a1d1002fc7
Rick <rickatherton316@gmail.com>
2022-08-31 09:07:29 +0000

    Add contents to bag context

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -8,8 +10,20 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
+    # Calculating product cost
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += product.price * quantity
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
+    # Calculating delivery cost
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
@@ -19,6 +33,7 @@ def bag_contents(request):
 
     grand_total = delivery + total
 
+    # Items to return in context
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -29,6 +44,4 @@ def bag_contents(request):
         'grand_total': grand_total,
     }
 
-    context = {}
-
-    return context 
+    return context
