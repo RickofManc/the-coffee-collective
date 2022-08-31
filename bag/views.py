4648b491121a4f66b95b9115ece8ea6777744cad
Rick <rickatherton316@gmail.com>
2022-08-31 14:56:29 +0000

    Add functionality to adjust bag item quantities

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,7 @@
 """
 Django imports to support Views
 """
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def view_bag(request):
@@ -37,3 +37,28 @@ def add_to_bag(request, item_id):
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the product quantities within the bag """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+    else:
+        if quantity > 0:
+            bag[item_id]= quantity
+        else:
+            bag.pop[item_id]
+    
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
