d1e79f0c2fdbf2c967c8c54cb8d8d18f1fef7244
Rick <rickatherton316@gmail.com>
2022-08-31 15:27:11 +0000

    Added remove from bag function and, calc subtotal template filter

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,10 @@
+from django import template
+
+
+register = template.Library()
+
+# Register filter on templates
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    """ Calculate subtotal within bag """
+    return price * quantity
