c529853e74c2d3c534631cf33ff69fdf16821e72
Rick <rickatherton316@gmail.com>
2022-08-31 12:00:50 +0000

    Finish add to bag logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -13,15 +13,27 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     # Calculating product cost
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += product.price * quantity
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     # Calculating delivery cost
     if total < settings.FREE_DELIVERY_THRESHOLD:
