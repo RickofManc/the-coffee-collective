3f1ae7db15bdffffe50a2244707b5b7e51768c76
Rick <rickatherton316@gmail.com>
2022-09-01 19:13:21 +0000

    Added Forms, with Models, Signals and Config

--- checkout/models.py
+++ checkout/models.py
@@ -20,9 +20,12 @@ class Order(models.Model):
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = models.CharField(max_length=40, null=False, blank=False)
     order_date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    delivery_cost = models.DecimalField(max_digits=6, 
+                                        decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10,
+                                      decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10,
+                                      decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
         """
@@ -55,14 +58,15 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
-
-
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product,
+                                null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=6, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                         null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
