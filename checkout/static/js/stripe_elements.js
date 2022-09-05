7535ce91a8d255314c0a65b6480ef39c1bd4e10b
Rick <rickatherton316@gmail.com>
2022-09-05 09:04:16 +0000

    Update font format to fix buintialising bug

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -14,7 +14,7 @@ var style = {
         color: '#282828',
         fontFamily: '"Open Sans", sans-serif',
         fontSmoothing: 'antialiased',
-        fontSize: '1rem',
+        fontSize: '16px',
         '::placeholder': {
             color: '#aab7c4'
         }
@@ -52,6 +52,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -65,6 +67,8 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
