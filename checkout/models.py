b5fb2b944b49520a8075ea67875097012f7f5329
Rick <rickatherton316@gmail.com>
2022-09-06 08:52:13 +0000

    Update form with conventional country field

--- checkout/models.py
+++ checkout/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-
+from django_countries.fields import CountryField
 from products.models import Product
 
 
@@ -17,7 +17,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     order_date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6,
                                         decimal_places=2, null=False,
