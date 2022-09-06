61f32ff9da813ce3509b6be49166322aedd20aa0
Rick <rickatherton316@gmail.com>
2022-09-06 08:53:32 +0000

    Update country field and ensure placeholder working

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,7 +8,7 @@ class OrderForm(forms.ModelForm):
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'county', 'postcode', 'country',
-        )
+                  )
 
     def __init__(self, *args, **kwargs):
         """
@@ -24,16 +24,16 @@ class OrderForm(forms.ModelForm):
             'street_address2': 'Street Address 2',
             'town_or_city': 'Town or City',
             'county': 'County',
-            'postcode': 'Postal Code',
-            'country': 'Country',         
+            'postcode': 'Postcode',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
