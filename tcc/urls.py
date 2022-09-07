2b70163a4da3a67b6ee44cceeeef3438e4c7d614
Rick <rickatherton316@gmail.com>
2022-09-07 19:20:11 +0000

    Add path for Profiles views

--- tcc/urls.py
+++ tcc/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
