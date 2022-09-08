8f304633101fe2da44fcf9ec60c338cf72f402ed
Rick <rickatherton316@gmail.com>
2022-09-08 15:59:32 +0000

    Update view to render form for user edits

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
+from django.contrib import messages
+
 
 def profile(request):
-    """ Display the users profile """
-
+    """ Display the users profile with order history """
+    
     profile = get_object_or_404(UserProfile, user=request.user)
+    # Function to save changes to User Profile
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
