9bdda3d7d9aaaee996f4c725b0519dc779d4a8a4
Rick <rickatherton316@gmail.com>
2022-09-08 20:14:00 +0000

    Add a view to display Order's from Profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
-from django.contrib import messages
+from checkout.models import Order
 
 
 def profile(request):
-    """ Display the users profile with order history """
+    """ Display the users profile with orders """
     
     profile = get_object_or_404(UserProfile, user=request.user)
     # Function to save changes to User Profile
@@ -23,6 +24,24 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """ Display the users order history """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}.'
+        'A confirmation was sent on the order date.'
+        ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
