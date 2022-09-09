6b3e38d34a2f139f7cdce3c11ebf10475d982476
Rick <rickatherton316@gmail.com>
2022-09-09 13:47:16 +0000

    Add message handling for changes to User Profile

--- profiles/views.py
+++ profiles/views.py
@@ -14,9 +14,12 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+            messages.success(request, 'Profile successfully updated')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is completed correctly')
+    else:
+        form = UserProfileForm(instance=profile)
+    
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
