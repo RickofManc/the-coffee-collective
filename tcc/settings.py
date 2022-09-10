7bbe5c95edd2cbf592a0c9c88bbd701f05c8895b
Rick <rickatherton316@gmail.com>
2022-09-10 14:45:35 +0000

    Finalise deployment to Heroku

--- tcc/settings.py
+++ tcc/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-coffee-collective', 'localhost']
 
 # Cross-Origin Resource Sharing to support third party resources to load
 X_FRAME_OPTIONS = 'SAMEORIGIN'
@@ -131,13 +131,17 @@ WSGI_APPLICATION = 'tcc.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
