19b6e75b3a2b60c8a0c86c9040850acb7e7dbf60
Rick <rickatherton316@gmail.com>
2022-09-10 15:13:45 +0000

    Update secret key and set debug

--- tcc/settings.py
+++ tcc/settings.py
@@ -26,10 +26,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-coffee-collective', 'localhost']
 
