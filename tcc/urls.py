c178498e9d9b0f460de83fe22fb2f8208bf03476
Rick <rickatherton316@gmail.com>
2022-09-12 13:38:29 +0000

    Add paths for 404/500 error pages

--- tcc/urls.py
+++ tcc/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,3 +17,7 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# override the standard 404 and 500 handlers
+handler404 = 'tcc.views.error_404'
+handler500 = 'tcc.views.error_500'
