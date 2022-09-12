78ad56eb07aa0a8240ba5ced5b80a99a70715228
Rick <rickatherton316@gmail.com>
2022-09-12 11:15:24 +0000

    Add settings for Email in production

--- tcc/settings.py
+++ tcc/settings.py
@@ -210,8 +210,17 @@ if 'USE_AWS' in os.environ:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'thecoffeecollective@gmail.com'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'thecoffeecollective1972@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER =  os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.egt('EMAIL_HOST_USER')
 
 # Account Authentication
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
