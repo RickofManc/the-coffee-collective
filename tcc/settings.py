375be635191d8f4db5b13d385b70c0f04c65e048
Rick <rickatherton316@gmail.com>
2022-09-12 10:29:02 +0000

    Add cache control

--- tcc/settings.py
+++ tcc/settings.py
@@ -185,8 +185,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS S3 Bucket
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'the-coffee-collective'
-    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
