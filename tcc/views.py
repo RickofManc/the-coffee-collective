b32f3978fdcaaad1e72ed196d108b54a3af2945a
Rick <rickatherton316@gmail.com>
2022-09-21 13:19:00 +0000

    Correct typo in function name

--- tcc/views.py
+++ tcc/views.py
@@ -6,6 +6,6 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 
-def handler404(request, exception):
+def handler500(request):
     """ Error Handler 500 - Page Not Found """
     return render(request, "errors/500.html", status=500)
