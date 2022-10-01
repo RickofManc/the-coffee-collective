f09fbb841f7d2f5896cc212e30f25a4199f16799
Rick <rickatherton316@gmail.com>
2022-10-01 15:12:52 +0000

    Fix pylint error with line too long

--- products/views.py
+++ products/views.py
@@ -44,7 +44,7 @@ def all_products(request):
                 messages.error(request,
                                "Please enter a search criteria")
                 return redirect(reverse('products'))
-            queries = Q(name__icontains=query) | Q(subtitle__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'(sort)_(direction)'
