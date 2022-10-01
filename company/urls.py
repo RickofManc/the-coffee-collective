2e2dd5d65409a3814bf0640e287329134b5a581d
Rick <rickatherton316@gmail.com>
2022-10-01 16:08:31 +0000

    Create path for Copytright Statement view

--- company/urls.py
+++ company/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("accessibility_statement/", views.AccessibilityStatement.as_view(), name="accessibility_statement",),
+    path("copyright_statement/", views.CopyrightStatement.as_view(), name="copyright_statement",),
 ]
