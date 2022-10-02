c52a7746144183ad5f56fa01009b37af226523a3
Rick <rickatherton316@gmail.com>
2022-10-02 10:42:37 +0000

    Create path for T&Cs view

--- company/urls.py
+++ company/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("accessibility_statement/", views.AccessibilityStatement.as_view(), name="accessibility_statement",),
     path("copyright_statement/", views.CopyrightStatement.as_view(), name="copyright_statement",),
+    path("terms_and_conditions/", views.TermsAndConditions.as_view(), name="terms_and_conditions",),
 ]
