59c00ef2e62fae6b8c1bb0766c96ea84f069639a
Rick <rickatherton316@gmail.com>
2022-10-02 18:45:42 +0000

    Create path to display the Sustainability view

--- company/urls.py
+++ company/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("accessibility_statement/", views.AccessibilityStatement.as_view(), name="accessibility_statement",),
     path("copyright_statement/", views.CopyrightStatement.as_view(), name="copyright_statement",),
     path("terms_and_conditions/", views.TermsAndConditions.as_view(), name="terms_and_conditions",),
+    path("sustainability/", views.Sustainability.as_view(), name="sustainability",),
 ]
