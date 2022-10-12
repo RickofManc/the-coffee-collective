c494400d5af70f9332346eda3ee1abd20abe6b0b
Rick <rickatherton316@gmail.com>
2022-10-12 21:11:18 +0000

    Add admin section for Contact form

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Displays the fields and values
+    for Contact Model in Django admin.
+    """
+    list_display = (
+        'name', 'email', 'subject',
+        'message', 'date_submitted'
+        )
+    search_fields = [
+        'name', 'subject', 'message',
+        'date_submitted'
+        ]
+    list_filter = (
+        'name', 'date_submitted'
+        )
