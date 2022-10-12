85a68442e4b2098678a4555e1e32c0484fdcfbac
Rick <rickatherton316@gmail.com>
2022-10-12 21:15:26 +0000

    Migrate new Contact model

--- company/models.py
+++ company/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Models the fields for the Contact Form.
+    """
+    name = models.CharField(max_length=155)
+    email = models.EmailField(max_length=155)
+    subject = models.CharField(max_length=200)
+    message = models.CharField(max_length=2000)
+    date_submitted = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        """ Set verbose name """
+        verbose_name = 'Contact Form'
+        verbose_name_plural = 'Contact Forms'
