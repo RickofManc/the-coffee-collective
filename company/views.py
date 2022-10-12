4b1f90d0ab8a31db3bad2667de1f9e0823c74ce5
Rick <rickatherton316@gmail.com>
2022-10-12 21:21:54 +0000

    Create view to render and execute Contact form

--- company/views.py
+++ company/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import CreateView
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.conf import settings
+from .forms import ContactForm
 
 
 class AccessibilityStatement(CreateView):
@@ -72,3 +76,41 @@ class OurStory(CreateView):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {})
+
+
+def contact(request, *args, **kwargs):
+    """
+    Displays Contact Us page form.
+    Uses Post method to send contact form.
+    Validation checks performed on input before saving.
+    Email sent externally to TCC Gmail.
+    Retains user on same page after commenting.
+    """
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name'],
+            email = form.cleaned_data['email'],
+            subject = form.cleaned_data['subject'],
+            message = form.cleaned_data['message'],
+           
+            # send mail combining field forms
+            send_mail({subject}, f'{name}, {email}, {message}',
+                      settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER],
+                      fail_silently=False)
+            messages.success(
+                request, 'Thank you for contacting us \
+                - we will reply within 24 hours!')
+           
+            # redirect to home page
+            return redirect(reverse('home'))
+        else:
+            messages.error(
+                request, "Something went wrong with your submission.\
+                Please try again."
+            )
+    # blank form created if any other method is used
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
