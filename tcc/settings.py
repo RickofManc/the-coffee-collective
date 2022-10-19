5a402a86b9b336750b04b6bcabd2ec9647fb1c12
Rick <rickatherton316@gmail.com>
2022-10-19 19:07:08 +0000

    Change debug to true to investigate issue in prod

--- tcc/settings.py
+++ tcc/settings.py
@@ -29,7 +29,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['the-coffee-collective.herokuapp.com', 'localhost']
 
