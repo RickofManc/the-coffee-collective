d6c340e65d00b9896f8268a8d886a478b1b2791e
Rick <rickatherton316@gmail.com>
2022-11-10 21:22:26 +0000

    Set Debug to False to test error pages

--- tcc/settings.py
+++ tcc/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['the-coffee-collective.herokuapp.com', 'localhost']
 
