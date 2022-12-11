0e374e4ad5e7b0f4790cbdf556fd13973e0c6802
Rick <rickatherton316@gmail.com>
2022-12-11 20:34:18 +0000

    Update paths as part of fixing bug B1

--- products/urls.py
+++ products/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-    path('delete/<int:product_id>/', views.DeleteProductView.as_view(),
-         name='delete_product_page'),
+    path('delete/<int:product_id>/',
+         views.delete_product, name='delete_product'),
 ]
