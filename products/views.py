6e7074d1d44f1d544fb6bc7ecd588c6ea74a0644
Rick <rickatherton316@gmail.com>
2022-12-11 21:46:33 +0000

    Remove unneccessary whitespace within function

--- products/views.py
+++ products/views.py
@@ -161,7 +161,7 @@ def delete_product(request, product_id):
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
-    
+
     if request.method == 'POST':
         product.delete()
         messages.success(request, f'{product.name} successfully deleted.')
