53de5422f0c8f40990350b935ff7f895f2330f55
Rick <rickatherton316@gmail.com>
2022-12-13 22:16:50 +0000

    Add URL path to view Wish List template

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('single_order/<order_number>',
          views.single_order, name='single_order'),
+    path('wishlist/', views.user_wish_list, name="wishlist"),
 ]
