4aeb70e21dbfcab93a91ba1eb559797c8cece3e1
Rick <rickatherton316@gmail.com>
2022-12-14 20:40:32 +0000

    Develop function to add products to Wish List

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile, UserWishList
@@ -60,5 +60,34 @@ def user_wish_list(request):
     if request.user.is_authenticated:
         return render(request, 'profiles/wishlist.html')
     else:
-        messages.error(request, 'Sorry you must be a member to have a Wish List.')
+        messages.error(request, 'Sorry you must be \
+            signed in to use a Wish List.')
         return redirect('home')
+
+
+def add_to_wishlist(request, pk):
+    """ view to display orders to user """
+    # check to see if the user is signed in
+    if request.user.is_authenticated:
+        # retrieve user and product models
+        user = UserProfile.objects.get(user=request.user)
+        product = get_object_or_404(Product, pk=pk)
+        # check to see if user has added the item to their wish list
+        if UserWishList.objects.filter(user=user, product=product).exists():
+            wishlist_item = UserWishList.objects.get(
+                user=user, product=product)
+            wishlist_item.delete()
+            messages.info(request, "removed from Wish List")
+            return redirect(reverse(
+                'all_products'), kwargs={'not_shopping': True})
+        else:
+            # if item is not in wish list then add the item
+            wishlist_item = UserWishList.objects.create(
+                user=user, product=product)
+            messages.success(request, f"{wishlist_item} added to Wish List")
+            return redirect(
+                reverse('products'), kwargs={'not_shopping': True})
+    else:
+        # redirect back to all products if the user isn't logged in
+        messages.error(request, 'You must be signed in to use a Wish List.')
+        return redirect(reverse('products'), kwargs={'not_shopping': True})
