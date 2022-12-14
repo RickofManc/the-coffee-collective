381efec273d5f3a9eddc6b9602eb67ff5dffc95a
Rick <rickatherton316@gmail.com>
2022-12-14 20:41:51 +0000

    Add url path for adding products to Wish List

--- profiles/urls.py
+++ profiles/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('single_order/<order_number>',
          views.single_order, name='single_order'),
     path('wishlist/', views.user_wish_list, name="wishlist"),
+    path('add_to_wishlist/<pk>', views.add_to_wishlist,
+          name="add_to_wishlist"),
 ]
