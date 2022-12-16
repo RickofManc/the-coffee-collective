6bb707681976b747220211ee056e04ca7403f26e
Rick <rickatherton316@gmail.com>
2022-12-16 20:22:52 +0000

    Update paths to the new Wish List views

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('single_order/<order_number>',
          views.single_order, name='single_order'),
-    path('wishlist/', views.user_wish_list, name="wishlist"),
-    path('add_to_wishlist/<pk>', views.add_to_wishlist,
-         name="add_to_wishlist"),
-    path('remove_from_wishlist/<pk>', views.remove_from_wishlist,
-         name="remove_from_wishlist")
+    path('wishlist/', views.wishlist, name='wishlist'),
+    path('wishlist/add_to_wishlist/<int:id>',
+         views.add_to_wishlist,
+         name='user_wishlist'),
 ]
