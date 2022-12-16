2f97e352917e804380c1be76f3584d0061c396d9
Rick <rickatherton316@gmail.com>
2022-12-16 20:28:39 +0000

    Update view function with new approach for Wish List

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile, UserWishList
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
 from products.models import Product
@@ -10,7 +11,6 @@ from products.models import Product
 @login_required
 def profile(request):
     """ Display the users profile with orders """
-
     profile = get_object_or_404(UserProfile, user=request.user)
     # Function to save changes to User Profile
     if request.method == 'POST':
@@ -32,7 +32,6 @@ def profile(request):
         'orders': orders,
         'on_profile_page': True
     }
-
     return render(request, template, context)
 
 
@@ -50,44 +49,31 @@ def single_order(request, order_number):
         'order': order,
         'from_profile': True,
     }
-
     return render(request, template, context)
 
 
 @login_required
-def user_wish_list(request):
-    """ Display the users wish list """
+def wishlist(request):
+    """ Display the users wish list page """
     if request.user.is_authenticated:
-        return render(request, 'profiles/wishlist.html')
+        products = Product.objects.filter(users_wishlist=request.user)
+        return render(request, 'profiles/wishlist.html',
+                      {'wishlist': products})
     else:
         messages.error(request, 'Sorry you must be \
             signed in to use a Wish List.')
         return redirect('home')
 
 
-def add_to_wishlist(request, pk):
-    """ view to display orders to user """
-    # check to see if the user is signed in
-    if request.user.is_authenticated:
-        # retrieve user and product models
-        user = UserProfile.objects.get(user=request.user)
-        product = get_object_or_404(Product, pk=pk)
-        # check to see if user has added the item to their wish list
-        if UserWishList.objects.filter(user=user, product=product).exists():
-            wishlist_item = UserWishList.objects.get(
-                user=user, product=product)
-            wishlist_item.delete()
-            messages.info(request, "removed from Wish List")
-            return redirect(reverse(
-                'all_products'), kwargs={'not_shopping': True})
-        else:
-            # if item is not in wish list then add the item
-            wishlist_item = UserWishList.objects.create(
-                user=user, product=product)
-            messages.success(request, f"{wishlist_item} added to Wish List")
-            return redirect(
-                reverse('products'), kwargs={'not_shopping': True})
+@login_required
+def add_to_wishlist(request, id):
+    product = get_object_or_404(Product, id=id)
+    if product.users_wishlist.filter(id=request.user.id).exists():
+        product.users_wishlist.remove(request.user)
+        messages.success(request, product.name + ' has been \
+                         removed from your Wish List')
     else:
-        # redirect back to all products if the user isn't logged in
-        messages.error(request, 'You must be signed in to use a Wish List.')
-        return redirect(reverse('products'), kwargs={'not_shopping': True})
+        product.users_wishlist.add(request.user)
+        messages.success(request, 'Added ' + product.name + ' to \
+                         your Wish List')
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
